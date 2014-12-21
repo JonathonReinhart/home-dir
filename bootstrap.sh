ed732b1876804011569a3d553ff53f4c4cd8770c
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2014-12-21 03:56:51 -0500

    Add bootstrap.sh to install everything

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# https://github.com/udryan10/.dotfiles/blob/master/bootstrap.sh
+
+cd "$(dirname "${BASH_SOURCE}")"
+#git pull origin master
+
+function doIt() {
+    # Copy all of the intended files into ~
+	rsync \
+        --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "bootstrap.sh" \
+        --exclude "update.sh" \
+        --exclude "README.md" \
+        -av --no-perms . ~
+
+    # Activate the changes in this session
+    source ~/.bash_profile
+
+    # Git stuff
+    git config --global core.excludesfile '~/.gitignore'
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
