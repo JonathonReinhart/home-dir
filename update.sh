b77e685a1dacc9eadf2c6b0add384e109c705d0c
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2015-01-03 21:11:56 -0500

    Change update.sh to use rsync --existing
    The previous script only worked for file in the top-level directory.
    Files in subdirectories of ~ would incorrectly be copied to the
    top-level directory of this repo.
    
    Rsync is the right solution here. --existing copies only files that
    already exist (to avoid the entire home directory from being copied!)

--- update.sh
+++ update.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Updates this repository with the versions of these files in the home directory.
-
-for f in $(find . -type f -not -path './.git/*' -printf '%P\n'); do
-    if [ -f ~/$f ]; then
-        cp -v ~/$f .
-    fi
-done
+rsync -av --no-perms --existing ~/ .
